45c9541b97c2305d2421ac39940a182f83ee6a55
Ivan Greguric Ortolan <ivan.g.ortolan@gmail.com>
2016-11-09 21:54:43 +0100

    added more styling, add/delete message buttons

--- app/app.component.js
+++ app/app.component.js
@@ -1,26 +1,42 @@
 "use strict";
 var core_1 = require('@angular/core');
+var rxjs_1 = require('rxjs');
 var feathers_service_1 = require('./feathers.service');
 var AppComponent = (function () {
-    function AppComponent(FeathersService, _ref) {
+    function AppComponent(FeathersService, _ref, _zone) {
         this.FeathersService = FeathersService;
         this._ref = _ref;
+        this._zone = _zone;
         this._messagesService = this.FeathersService.service('messages');
     }
     AppComponent.prototype.ngOnInit = function () {
         var _this = this;
         this._messagesService.find()
             .subscribe(function (resp) {
-            _this._messages = resp.data;
-            _this._ref.detectChanges();
+            _this._zone.run(function () {
+                _this._messages = resp;
+            });
         });
     };
+    AppComponent.prototype.create = function () {
+        var text = 'Text' + Math.random().toString(36).substr(2, 5);
+        this._messagesService.create({
+            text: text
+        });
+    };
+    AppComponent.prototype.remove = function (id) {
+        this._messagesService.remove(id);
+    };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', rxjs_1.Observable)
+    ], AppComponent.prototype, "_messages", void 0);
     AppComponent = __decorate([
         core_1.Component({
             selector: "app",
             templateUrl: "app.component.html",
         }), 
-        __metadata('design:paramtypes', [feathers_service_1.FeathersService, core_1.ChangeDetectorRef])
+        __metadata('design:paramtypes', [feathers_service_1.FeathersService, core_1.ChangeDetectorRef, core_1.NgZone])
     ], AppComponent);
     return AppComponent;
 }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
-import {Observable} from 'rxjs';
-import {FeathersService} from './feathers.service';
+import { Component, OnInit, ChangeDetectorRef, Input, NgZone } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FeathersService } from './feathers.service';
 
 @Component({
     selector: "app",
@@ -9,18 +9,31 @@ import {FeathersService} from './feathers.service';
 export class AppComponent implements OnInit {
 
     private _messagesService: any;
-    private _messages: Observable<any>;
+    @Input() private _messages: Observable<any>;
 
     constructor(private FeathersService: FeathersService,
-                private _ref: ChangeDetectorRef) {
+        private _ref: ChangeDetectorRef,
+        private _zone: NgZone) {
         this._messagesService = this.FeathersService.service('messages');
     }
 
     ngOnInit(): void {
         this._messagesService.find()
             .subscribe(resp => {
-                this._messages = resp.data;
-                this._ref.detectChanges();
+                this._zone.run(() => {
+                    this._messages = resp;
+                });
             });
+    }
+
+    create() {
+        let text = 'Text' + Math.random().toString(36).substr(2, 5);
+        this._messagesService.create({
+            text: text
+        });
+    }
+
+    remove(id: string) {
+        this._messagesService.remove(id);
     }
 }
